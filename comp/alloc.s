7bbe27631feb4aecb6cfcbbf66b4b36182dd2d70
Daniel5756 <danya.berkovich@gmail.com>
2026-03-19 16:19:44 -0400

    made scope system

--- comp/alloc.s
+++ comp/alloc.s
@@ -2,14 +2,19 @@
 lastScope: .long 0
 .text
 newScope:
-
-
+    //push last scope
+    //update lastScope to this scope
+    pushq lastScope
+    movq %rsp, lastScope
 ret
 alloc:
-
-
+    //push name, len, val
+    
+    
 ret
 endscope:
-
-
+    //%rsp = lastScope
+    //pop into lastscope
+    mov lastScope, %rsp
+    pop lastScope
 ret
